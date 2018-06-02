dc31920a095ea3a17ca2a69dfbf3cb85c722c249
Kubo <t_nutji@hotmail.com>
2018-06-02 15:11:21 +0700

    For & While Loop

---
+++ Loop.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//For Loop
+
+var friendString = ["AAA","BBB","CCC"]
+
+for loveFriendString in friendString {
+    print("LoveFriend ==> \(loveFriendString)")
+}
+
+
+//While Loop
+
+let maxInt: Int = 10
+var startInt:Int = 0
+
+while startInt <= maxInt {
+    
+    print("StartInt ==> \(startInt)")
+    startInt += 1
+}
