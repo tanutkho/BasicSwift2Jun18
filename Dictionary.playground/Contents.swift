e53ff41ddcb59e21825a948d769bad79f932c454
Kubo <t_nutji@hotmail.com>
2018-06-02 11:57:04 +0700

    Dictionary

---
+++ Dictionary.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Dictionary
+
+//การกำหนดค่า
+
+var nameDicString = ["key1":"Doramon","key2":"Nobita","key3":"Giant"]
+var numberDicInt = ["num1":11, "num2":22]
+
+//การเรียกใช้
+print("ค่าของ nameDicString ที่มีค่า key = key2 คือ \(nameDicString["key2"])")
+
+print("number ==> \(numberDicInt["num1"])")
+
+
+
+
