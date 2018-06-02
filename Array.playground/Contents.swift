502f27ccbd73a555317c13e80a7ace3f8fbf2777
Kubo <t_nutji@hotmail.com>
2018-06-02 11:41:23 +0700

    Array

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,33 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Array
+
+var numbersInt = [11,22,33]
+//การหาจำนวนสมาชิกของ Array
+print("จำนวนสมาชิก => \(numbersInt.count)")
+
+//การเพิ่มสมาชิกบน Array
+numbersInt.append(55)
+
+//แสดงสมาชิกที่ 3
+print("สมาชิกที่ 3 มีค่า ==> \(numbersInt[3])")
+
+//การลบสมาชิก
+numbersInt.remove(at: 1)
+numbersInt
+numbersInt[2]
+
+numbersInt.append(8)
+numbersInt.append(15)
+
+//การเรียงข้อมูลจากน้อยไปมาก
+numbersInt.sort()
+numbersInt
+numbersInt[2]
+
+
+
+
+
