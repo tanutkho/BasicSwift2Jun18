b9a1260c682d2b31577bb6c082f05120b59674c4
Kubo <t_nutji@hotmail.com>
2018-06-02 10:06:18 +0700

    Variable and Constant

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Variable and Constant
+
+//Implicit Variable
+//การประกาศตัวแปรแบบใช้ค่าเป็นตัวกำหนด Data Type
+
+var nameString = "Kubo"
+var numberInt = 123
+var numberDouble = 3.1416
+var StatusBol = true
+
+//Try To Change
+nameString = "Doramon"
+numberInt = 555
+
+//Implicit Constant การประกาศค่าคงที่ แก้ไขไม่ได้
+
+let surnameString = "Khositprasert"
+
+
+
+
+
+
+
+
+
+
+
