8d96f39e9eabda025859a2ab855593d4a9a4d0c0
Kubo <t_nutji@hotmail.com>
2018-06-03 10:29:46 +0700

    Optional

---
+++ Optional.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var number1Int: Int?
+
+//print("number1 ==> \(number1Int)")
+
+var numberString: String = "five"
+
+
+
+// var numberInt: Int = Int(numberString)!
+//
+// print("My Number ==> \(numberInt)")
+
+
+
+if var testNumberInt = Int(numberString) {
+    print("Work! = \(testNumberInt)")
+}
+else {
+    print("Cannot work!")
+}
+
+
+
